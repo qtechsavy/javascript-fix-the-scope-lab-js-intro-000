d702a0d4d4a4eef3036733d08605fadb4eb692c0
Gunu Shrestha <realgunu@gmail.com>
2017-12-02 05:11:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function add2(n) {
 }
 
 var theFunk = funkyFunction();
-  function funkyFunction(`FUNKY!`){
+  function funkyFunction(FUNKY!){
   return funkyFunction;
   }
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
