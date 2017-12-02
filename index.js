a72df008e6b54df509f02e3346f3df164f61b020
Gunu Shrestha <realgunu@gmail.com>
2017-12-02 05:22:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ function myAnimal() {
   var animal = 'dog';
   return animal;
 }
-describe('yourAnimal()', function()){
-  it('returns your animal', function()){
-    expect (window.yourAnimal()).toEqual('cat')
-  }
-}
-
+ 
 function yourAnimal() {
   var animal = 'cat';
   // How can we make sure that this function
@@ -18,7 +13,7 @@ function yourAnimal() {
 
 function add2(n) {
   var two = 2;
-  return (n + 2);
+  return (n + two);
 
   // Feel free to move things around!
   const two = 2
