d50f328686c56f5f41a67b04697346754027060e
Gunu Shrestha <realgunu@gmail.com>
2017-12-02 05:20:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var animal = 'dog';
-
 function myAnimal() {
-  var animal = 'cat';
+  var animal = 'dog';
   return animal;
+}
+describe('yourAnimal()', function()){
+  it('returns your animal', function()){
+    expect (window.yourAnimal()).toEqual('cat')
+  }
 }
 
 function yourAnimal() {
+  var animal = 'cat';
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal();
+  return animal;
 }
 
 function add2(n) {
+  var two = 2;
   return (n + 2);
 
   // Feel free to move things around!
